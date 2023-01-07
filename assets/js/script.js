d120b006e3141f538558fff65973b8879990319a
Clarice Tran <tran.clarice@gmail.com>
2023-01-07 04:47:58 -0800

    Added interval function to update color coded sections based on hour. Added function to load saved events to appropriate time blocks.

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,8 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
-	var businessHours = [9, 17]; // start time at 9 and end hour at 5PM (17).
+	// start time at 9AM and end hour at 5PM (17).
+	var businessHours = [9, 17];
 	// Example of how bootstrap html looks for the timeblocks:
 	//	<div id="hour-9" class="row time-block past">
 	// 		<div class="col-2 col-md-1 hour text-center py-3">9AM</div>
@@ -22,24 +23,44 @@ $(function () {
 				'</div><textarea class="col-8 col-md-10 description" rows="3"></textarea><button class="btn saveBtn col-2 col-md-1" aria-label="save"><i class="fas fa-save" aria-hidden="true"></i></button></div>'
 		);
 	}
-
 	// Listener for click events on save buttons.
 	$(".saveBtn").click(function () {
 		var timeblockEl = $(this).parent();
 		// directly setting the hour-x as the key to grab by using a computed property name
 		localStorage.setItem([timeblockEl.attr("id")], timeblockEl.find("textarea").val());
 	});
-	// TODO: Add code to apply the past, present, or future class to each time
-	// block by comparing the id to the current hour. HINTS: How can the id
-	// attribute of each time-block be used to conditionally add or remove the
-	// past, present, and future classes? How can Day.js be used to get the
-	// current hour in 24-hour time?
-	//
-	// TODO: Add code to get any user input that was saved in localStorage and set
-	// the values of the corresponding textarea elements. HINT: How can the id
-	// attribute of each time-block be used to do this?
-	//
-	// TODO: Add code to display the current date in the header of the page.
+
+	// Every second, the site will check the current hour compared to the listed business hours and determine if the event is in the past, present, or future and color code accordingly.
+	setInterval(function () {
+		var currHour = dayjs().hour();
+		for (var time = businessHours[0]; time <= businessHours[1]; time++) {
+			if (time < currHour) {
+				$("#hour-" + time)
+					.addClass("past")
+					.removeClass("present future");
+			} else if (time == currHour) {
+				$("#hour-" + time)
+					.addClass("present")
+					.removeClass("past future");
+			} else {
+				$("#hour-" + time)
+					.addClass("future")
+					.removeClass("past present");
+			}
+		}
+	}, 1000);
+
+	// Places any saved events into their respective timeblocks.
+	for (var timeBlock = businessHours[0]; timeBlock <= businessHours[1]; timeBlock++) {
+		var savedText = localStorage.getItem("hour-" + timeBlock);
+		if (savedText != null) {
+			$("#hour-" + timeBlock)
+				.find("textarea")
+				.text(savedText);
+		}
+	}
+
+	//Displays the current date in the header of the page.
 	dayjs.extend(window.dayjs_plugin_advancedFormat);
 	$("#currentDay").text(dayjs().format("dddd, MMMM Do"));
 });
